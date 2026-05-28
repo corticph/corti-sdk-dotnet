bea9bfea2c15dd423b42be932ee4b3dc8c45df0a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-05-28 08:19:34 +0000

    SDK regeneration

---
+++ src/Corti/Types/LanguagesListResponse.cs
@@ -0,0 +1,28 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Corti.Core;
+
+namespace Corti;
+
+[Serializable]
+public record LanguagesListResponse : IJsonOnDeserialized
+{
+    [JsonExtensionData]
+    private readonly IDictionary<string, JsonElement> _extensionData =
+        new Dictionary<string, JsonElement>();
+
+    [JsonPropertyName("languages")]
+    public Dictionary<string, object?> Languages { get; set; } = new Dictionary<string, object?>();
+
+    [JsonIgnore]
+    public ReadOnlyAdditionalProperties AdditionalProperties { get; private set; } = new();
+
+    void IJsonOnDeserialized.OnDeserialized() =>
+        AdditionalProperties.CopyFromExtensionData(_extensionData);
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return JsonUtils.Serialize(this);
+    }
+}
